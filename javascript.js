517787ddc5a3d33024ad037de1727c06aeebec3d
Reed112485 <139658839+Reed112485@users.noreply.github.com>
2023-08-19 12:28:42 -0700

    Add function/comparisons, determine winner

--- javascript.js
+++ javascript.js
@@ -4,17 +4,25 @@ Get choice of computer
  - Assign a choice to each number with an if statement
  - return the choice
  
- Play a round
- - get playerSelection input
- - use computerSelection choice
- - use comparison to determine the winner
- - output a message saying who won
+ Get choice of player
+ - prompt for choice
+ - use if statement to determine choice
+ - return the choice
+
+ Compare choices for a winner
+ - assign computer choice to a variable by calling function, using return value
+ - assign player choice to a variable by calling function, using return value
+
+Keep track of the score
+- assign winner function to a variable
+- use that variable to accrue a score for each player
+-- if the winner was cpu, add to cpu score. Otherwise add to p1 score.
+- return the scores
  
- Play five rounds
- - loop through the game with a for statement
- - set score keeping variables
- - increase the score per the winner
- - output the winner
+ Play five rounds and then terminate
+ - loop through the game five times with a for statement
+ - use score keeping function to keep score
+ - output the winner and the scores
  */
 
  function getComputerChoice() {
@@ -30,7 +38,7 @@ Get choice of computer
  }
 
 function getPlayerChoice() {
-    let choice = prompt ("Press 1 for Rock, 2 for Paper, or 3 for Scissors: ");
+    let choice = prompt ("Press 1 for Rock, 2 for Paper, or 3 for Scissors ");
 
     if (choice === "1") {
         return "Rock";
@@ -41,13 +49,40 @@ function getPlayerChoice() {
     }
 }
 
-function playRound(getComputerChoice, getPlayerChoice) {
+function playRound(computerChoice, playerChoice) {
 
-    if (getComputerChoice === "Rock" && getPlayerChoice === "Paper") {
-        alert (`You picked ${getPlayerChoice}, CPU picked ${getComputerChoice}. You win!`);
-    } else if (getComputerChoice === "Paper" && getPlayerChoice === "Scissors") {
-        alert (`You picked ${getPlayerChoice}, CPU picked ${getComputerChoice}. You win!`);
-    } else if (getComputerChoice === "Scissors" && getPlayerChoice === "Rock") {
-        alert (`You picked ${getPlayerChoice}, CPU picked ${getComputerChoice}. You win!`);
-    } else if ()
+    if (computerChoice === "Rock" && playerChoice === "Rock") {
+        console.log(`Player chose Rock, CPU chose Rock.`);
+        return "It's a Tie!";
+    } else if (computerChoice === "Rock" && playerChoice === "Paper") {
+        console.log(`Player chose Paper, CPU chose Rock.`);
+        return "Player wins!";
+    } else if (computerChoice === "Rock" && playerChoice === "Scissors") {
+        console.log(`Player chose Scissors, CPU chose Rock.`);
+        return "CPU wins!";
+    }
+
+    if (computerChoice === "Paper" && playerChoice === "Rock") {
+        console.log(`Player chose Rock, CPU chose Paper.`);
+        return "CPU wins!";
+    } else if (computerChoice === "Paper" && playerChoice === "Paper") {
+        console.log(`Player chose Paper, CPU chose Paper.`);
+        return "It's a Tie!";
+    } else if (computerChoice === "Paper" && playerChoice === "Scissors") {
+        console.log(`Player chose Scissors, CPU chose Paper.`);
+        return "Player wins!";
+    }
+
+    if (computerChoice === "Scissors" && playerChoice === "Rock") {
+        console.log(`Player chose Rock, CPU chose Scissors.`);
+        return "Player wins!";
+    } else if (computerChoice === "Scissors" && playerChoice === "Paper") {
+        console.log(`Player chose Paper, CPU chose Scissors.`);
+        return "CPU wins!";
+    } else if (computerChoice === "Scissors" && playerChoice === "Scissors") {
+        console.log(`Player chose Scissors, CPU chose Scissors.`);
+        return "It's a Tie!";
+    }
 }
+
+playRound(getComputerChoice(), getPlayerChoice());
