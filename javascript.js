7a059f51c5d1b69520f4bc6355412c66a7a7aa15
Reed112485 <139658839+Reed112485@users.noreply.github.com>
2024-09-20 14:18:41 -0700

    Start project fresh
    Got up to the point of getting one round to work. Test for length of
    text.

--- javascript.js
+++ javascript.js
@@ -1,79 +1,59 @@
-/* 
-Get a random choice:
-- create an array of the choices
-- create a variable that chooses a random number(index) based on the array length
-- return the array index that was randomly chosen
+let humanScore = 0;
+let computerScore = 0;
 
-Create a playRound function:
-- use the player choice and cpu choice as parameters
-- use an if statement
-- if the player choice is equal to the cpu choice, return tie
-- if any of the player win conditions are true, return player, use OR
-- in any other case, return cpu
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
 
-Create a playFive or playGame or whatever function:
-- create a variable for the player score
-- create a variable for the cpu score
+    if (choice == 0) {
+        choice = `rock`;
+    } else if (choice == 1) {
+        choice = `paper`;
+    } else if (choice == 2) {
+        choice = `scissors`
+    }
+    console.log(`CPU chose: ` + choice);
 
-- create a for loop which loops five times
--- assign a variable to the player choice which prompts for a choice and returns lower case
--- assign a variable to the cpu choice which uses the random choice function
--- create a result variable which plays a round, which will return the result
--- console.log the round number and what you and the cpu chose
-
-- create an if statement in the foor loop
--- if the result (contains return from playing a round) = player, increase the player score and console.log a message saying you won the round
--- if the result = cpu, increase the cpu score and write a message saying the cpu won
--- else, output that it was a draw
-
-- INSIDE the for but OUTSIDE the if:
--- log that the game has ended and output the scores
-
-Call the function which plays the game
-*/
-
-function getRandom() {
-    const choices = ["rock", "paper", "scissors"];
-    const random = Math.floor(Math.random() * choices.length);
-
-    return choices[random];
+    return choice;
 }
 
-function playRound(playerChoice, cpuChoice) {
-    if (playerChoice === cpuChoice) {
-        return "tie";
-    } else if (playerChoice === "rock" && cpuChoice === "scissors" ||
-               playerChoice === "paper" && cpuChoice === "rock" ||
-               playerChoice === "scissors" && cpuChoice === "paper") {
-        return "player";
-    } else {
-        return "cpu";
+function getHumanChoice() {
+    let choice = prompt(`Rock, Paper, or Scissors?`);
+    choice = choice.toLowerCase();
+
+    console.log(`You chose: ` + choice);
+
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == `rock` && computerChoice == `rock`) {
+        return console.log(`It's a tie!`);
+    } else if (humanChoice == `rock` && computerChoice == `paper`) {
+        return console.log(`You lose!`);
+        computerScore++
+    } else if (humanChoice == `rock` && computerChoice == `scissors`) {
+        return console.log(`You win!`);
+        humanScore++
+    } else if (humanChoice == `paper` && computerChoice == `rock`) {
+        return console.log(`You win!`);
+        humanScore++
+    } else if (humanChoice == `paper` && computerChoice == `paper`) {
+        return console.log(`It's a tie!`);
+    } else if (humanChoice == `paper` && computerChoice == `scissors`) {
+        return console.log(`You lose!`);
+        computerScore++
+    } else if (humanChoice == `scissors` && computerChoice == `rock`) {
+        return console.log(`You lose!`);
+        computerScore++
+    } else if (humanChoice == `scissors` && computerChoice == `paper`) {
+        return console.log(`You win!`);
+        humanScore++
+    } else if (humanChoice == `scissors` && computerChoice == `scissors`) {
+        return console.log(`It's a tie!`);
     }
 }
 
-function playFive() {
-    let playerScore = 0;
-    let cpuScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-    for (i = 0; i < 5; i++) {
-        const playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-        const cpuChoice = getRandom();
-
-        const result = playRound(playerChoice, cpuChoice);
-        console.log(`Round ${i + 1}: You chose: ${playerChoice} -- CPU chose: ${cpuChoice}`);
-
-        if (result === "player") {
-            playerScore++;
-            console.log(`You win!`);
-        } else if (result === "cpu") {
-            cpuScore++;
-            console.log(`CPU wins =(`);
-        } else {
-            console.log(`It's a tie!`)
-        }
-    }
-
-    console.log(`::Game Over::\nYour Score: ${playerScore}\nCPU Score: ${cpuScore}`);
-}
-
-playFive();
+playRound(humanSelection, computerSelection);
